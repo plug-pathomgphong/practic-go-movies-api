faed1980b1af9bdb3db56198a9d55810492f3e8c
plug <plug185@gmail.com>
2022-03-20 00:07:10 +0700

    Add: Middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
@@ -14,5 +14,5 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovie)
 
-	return router
+	return app.enableCORS(router)
 }
